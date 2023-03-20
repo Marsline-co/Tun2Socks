bb05d12975671f32ff22b12b38b66300fc91e240
niufuwei <niufuwei@163.com>
2023-03-20 22:03:26 +0800

    tun

--- Package.swift
+++ Package.swift
@@ -23,7 +23,7 @@ let package = Package(
     .binaryTarget(
         name: "HevSocks5Tunnel",
         url: "https://github.com/Cross-Kit/Tun2Socks/releases/download/2.1.10/HevSocks5Tunnel.xcframework.zip",
-        checksum: "b6a05ad21692769b1ea4d66bb27c5ca860b37d23f9e53dab642d0d361777778d"
+        checksum: "f5a0edb493fd689c871e109f47eb8959bbe02b4a7d94a827df97dd47e8a1bba6"
     )
   ]
 )
