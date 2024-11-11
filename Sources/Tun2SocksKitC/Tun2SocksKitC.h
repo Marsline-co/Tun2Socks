67f1f2610e6086b63b5fc9d0ce6ceff81da0a888
amir pirzad <pirzad7@gmail.com>
2024-11-11 17:35:33 +0200

    Update Tun2SocksKitC.h

--- Sources/Tun2SocksKitC/Tun2SocksKitC.h
+++ Sources/Tun2SocksKitC/Tun2SocksKitC.h
@@ -2,7 +2,12 @@
 #define Tun2SocksKitC_H
 
 #include <stdint.h>
-#include <sys/types.h>
+
+typedef uint8_t  u_int8_t;
+typedef uint16_t u_int16_t;
+typedef uint32_t u_int32_t;
+typedef uint64_t u_int64_t;
+typedef unsigned char u_char;
 
 #define CTLIOCGINFO 0xc0644e03UL
 
